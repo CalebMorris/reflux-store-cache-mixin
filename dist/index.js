e7ed3af8e6b88d64813e1406fcf63969256b5ab6
Caleb Morris <caleb.morris.g@gmail.com>
2015-07-06 03:07:24 -0700

    Added dist of package

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+var StoreCacheMixin = _interopRequire(require("./store-cache-mixin"));
+
+module.exports = StoreCacheMixin;
