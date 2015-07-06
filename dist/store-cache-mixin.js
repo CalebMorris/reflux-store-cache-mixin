46223cbb698b89ac6c5dd7819933895ac339b3e3
Caleb Morris <caleb.morris.g@gmail.com>
2015-07-06 04:45:33 -0700

    Update dist for v0.0.4

--- dist/store-cache-mixin.js
+++ dist/store-cache-mixin.js
@@ -1,20 +1,24 @@
 "use strict";
 
-var Map = require("immutable").Map;
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
 
-var _cache = require("./cache");
+var _ = _interopRequire(require("lodash"));
 
-var getTimestamp = _cache.getTimestamp;
-var hasExpired = _cache.hasExpired;
+var Immutable = _interopRequire(require("immutable"));
+
+var _util = require("./util");
+
+var getTimestamp = _util.getTimestamp;
+var hasExpired = _util.hasExpired;
 
 // Key not possible from plain JS
-var TIMESTAMPKEY = { _timestamp: "_timestamp" };
-var DATAKEY = { _data: "_data" };
+var TIMESTAMPKEY = "__timestamp";
+var DATAKEY = "__data";
 
 var StoreCacheMixin = {
 
   init: function init() {
-    this.cache = new Map();
+    this.cache = new Immutable.Map();
   },
 
   /*
@@ -35,6 +39,9 @@ var StoreCacheMixin = {
       throw new Error("Invalid usage: function(dataKey, instanceKey)");
     }
 
+    dataKey = Immutable.fromJS(dataKey);
+    instanceKey = Immutable.fromJS(instanceKey);
+
     if (this.cache.has(dataKey) && this.cache.get(dataKey).has(instanceKey) && !hasExpired(this.cache.get(dataKey).get(instanceKey).get(TIMESTAMPKEY), duration || this._ttl)) {
       return this.cache.get(dataKey).get(instanceKey).get(DATAKEY);
     }
@@ -53,7 +60,10 @@ var StoreCacheMixin = {
       throw new Error("Invalid usage: function(dataKey, instanceKey, data)");
     }
 
-    var newData = new Map().set(DATAKEY, data).set(TIMESTAMPKEY, getTimestamp());
+    dataKey = Immutable.fromJS(dataKey);
+    instanceKey = Immutable.fromJS(instanceKey);
+
+    var newData = new Immutable.Map().set(DATAKEY, data).set(TIMESTAMPKEY, getTimestamp());
 
     var oldData = null;
 
@@ -62,7 +72,7 @@ var StoreCacheMixin = {
     }
 
     if (!this.cache.has(dataKey)) {
-      this.cache = this.cache.set(dataKey, new Map());
+      this.cache = this.cache.set(dataKey, new Immutable.Map());
     }
 
     this.cache = this.cache.set(dataKey, this.cache.get(dataKey).set(instanceKey, newData));
@@ -81,6 +91,9 @@ var StoreCacheMixin = {
       throw new Error("Invalid usage: function(dataKey, instanceKey, data)");
     }
 
+    dataKey = Immutable.fromJS(dataKey);
+    instanceKey = Immutable.fromJS(instanceKey);
+
     var oldData = null;
 
     if (this.cache.has(dataKey) && this.cache.get(dataKey).has(instanceKey)) {
@@ -88,12 +101,33 @@ var StoreCacheMixin = {
     }
 
     if (!this.cache.has(dataKey)) {
-      this.cache = this.cache.set(dataKey, new Map());
+      this.cache = this.cache.set(dataKey, new Immutable.Map());
     }
 
     this.cache = this.cache.set(dataKey, this.cache.get(dataKey).set(instanceKey, null));
 
     return oldData;
+  },
+
+  /**
+   * Simplified usage
+   * @param  {*} dataKey - Which data to fetch from
+   * @param  {*} instanceKey - Which instance for that data to fetch
+   * @param  {Func<Obj?,Func<Objc>>} handler - Loader and saver callback
+   */
+  loadData: function loadData(dataKey, instanceKey, handler) {
+
+    if (!_.isFunction(handler)) {
+      throw new Error("Invalid handler: must be a function");
+    }
+
+    var data = this.fetchData(dataKey, instanceKey);
+
+    if (!data) {
+      return handler(null, this.storeData.bind(this, dataKey, instanceKey));
+    }
+
+    return handler(data, _.noop);
   } };
 
 module.exports = StoreCacheMixin;
